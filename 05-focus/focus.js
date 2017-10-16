260d75c705ca0396782dd7ceb14f5efedb1b3466
eveporcello <eve@moonhighway.com>
2017-10-15 18:51:44 -0700

    file update

--- 05-focus/focus.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	var list = $('ul'),
-		listItems = list.find('li');
-	$('.delete').on('click', function() {
-		$(this).parent().remove();
-		listItems.find('.delete').first().focus();
-	});
-});
